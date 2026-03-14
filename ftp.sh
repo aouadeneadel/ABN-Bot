38681b2a27b22b6b5e056ed2b4715787771a6dd4
AOUADENE Adel <aouadeneadel@outlook.fr>
2026-03-14 01:22:47 +0100

    Enhance ftp.sh with log checks and command updates
    Added log directory existence check and improved log file command construction.

--- ftp.sh
+++ ftp.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 . main.conf
 
+# Vérification que le répertoire de logs existe
+if [ ! -d "$logpath" ]; then
+    echo "Erreur : le répertoire de logs '$logpath' n'existe pas." >&2
+    exit 1
+fi
+
+# Construction de la liste des commandes put pour lftp
 logfiles=''
-
-for i in $(ls $logpath)
-do
-	logfiles="${logfiles}put $logpath/$i"
-	logfiles+=$'\n'
+for i in "$logpath"/*; do
+    [ -f "$i" ] || continue
+    logfiles="${logfiles}put \"${i}\""
+    logfiles+=$'\n'
 done
-echo -e $logfiles
 
+if [ -z "$logfiles" ]; then
+    echo "Aucun fichier à transférer dans '$logpath'." >&2
+    exit 0
+fi
 
-lftp -u $ftpuser,$ftppassword $ftphost <<EOF
+echo "$logfiles"
+
+lftp -u "$ftpuser","$ftppassword" "$ftphost" <<EOF
 set ssl:verify-certificate yes
 cd $ftpdirectory
 $logfiles
